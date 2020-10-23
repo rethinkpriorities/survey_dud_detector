203089c65ddeb24c9d2df70ee861243db4549ba3
Peter Hurford <peter@peterhurford.com>
2020-10-22 20:46:56 -0500

    Produce adjusted multiple low incidence

--- survey_dud_detector/dud_detector.py
+++ survey_dud_detector/dud_detector.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from collections import Counter
 
 
@@ -5,7 +7,7 @@ def detect_straightlining(df):
     return df.apply(lambda xs: max(Counter([str(x) for x in xs.values]).values()), axis=1) / df.shape[1]
 
 
-def detect_low_incidence(df, low_incidence_threshold=0.04):
+def detect_low_incidence(df, low_incidence_threshold=0.04, adjust=False):
     counts = None
 
     for c in df.columns:
@@ -14,5 +16,10 @@ def detect_low_incidence(df, low_incidence_threshold=0.04):
             counts = count_
         else:
             counts += count_
-    
-    return counts.astype(int)
+
+    if adjust:
+        counts = pd.Series([low_incidence_threshold ** c * len(df) for c in counts])
+        return pd.Series([1 if c >= 1 else c for c in counts]) 
+    else:
+        return counts.astype(int)
+
